6b288fe84a626b9721557fdd24af0ac67ea034e5
Oloo <olooine@gmail.com>
2024-12-01 19:54:06 +0300

    Congex get and create thumbnail

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,12 +1,38 @@
-import { SignInButton, SignOutButton } from "@clerk/nextjs";
-import { auth } from '@clerk/nextjs/server'
+"use client"
+import { SignInButton, SignOutButton, useSession } from "@clerk/nextjs";
+import { useMutation, useQuery } from "convex/react";
+import { api } from "../../convex/_generated/api"
+// import { auth } from '@clerk/nextjs/server'
 
 
-export default async function Home() {
-  const {userId} = await auth();
+export default function Home() {
+  const {isSignedIn} = useSession();
+  const createThumbnail = useMutation(api.thumbnails.createThumbnail)
+  const thumbnails = useQuery(api.thumbnails.getThumbnailsForUser)
   return (
     <main className="main">
-      {userId ? <SignOutButton /> : <SignInButton/>}
+      {isSignedIn ? <SignOutButton /> : <SignInButton/>}
+      {isSignedIn && (
+        <form onSubmit={async (e) => {
+          e.preventDefault();
+          const form = e.target as HTMLFormElement
+          const formData = new FormData(e.currentTarget)
+          const title = formData.get('title') as string
+          await createThumbnail({
+            title,
+          })
+          form.reset()
+        }}>
+          <label htmlFor="title">Title</label>
+          <input name="title" className="text-black"/>
+          <button>Create</button>
+        </form>
+      )}
+
+      <br/>
+      {thumbnails?.map((thumbnail) => {
+        return <div key={thumbnail._id}>{thumbnail.title}</div>
+      })}
     </main>
   );
 }
